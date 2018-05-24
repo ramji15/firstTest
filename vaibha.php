2a6bdc553a78d993dbe8f725f934b702891c5e37
rajvirlinux <rajvirlinux001@gmail.com>
2018-05-24 19:27:27 +0530

    vaibhav

---
+++ vaibha.php
@@ -0,0 +1,10 @@
+WPE SFTP
+IP = 104.196.192.109
+user = hitmaxelite-chetan
+pass = 97sqflo3OKmN
+port = 2222
+
+1. "shopifyInactiveOrder.php" (Every 24 hours)
+2. "shopifyOrderAll.php" (Every 24 hours)
+3. "shopifyOrder.php" (Every 30 min)
+Note: You can find these files in root folder, where "wp-load.php" file is available.
